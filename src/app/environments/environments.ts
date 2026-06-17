9c20b73d5be1a72cffaed5a8027565cfcf95ecc0
הדסה פישל <dasif7616@gmail.com>
2026-06-17 17:02:06 +0300

    Update API URL to production server

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://wolftasks-server.onrender.com/health'
+  production: true,
+  apiUrl: 'https://wolftasks-server.onrender.com/api'
 };
